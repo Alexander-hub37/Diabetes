70a5a6ede59f9523f4d37b55eda2b90d20e35852
unknown <u18302181@utp.edu.pe>
2022-07-05 09:37:30 -0500

    first commit

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ import seaborn as sns
 
 
 
-df = pd.read_csv(![](diabetes.csv))
+df = pd.read_csv(r'diabetes.csv')
 
 # HEADINGS
 st.title('Diabetes Checkup')
-st.sidebar.header('Patient Data')
+st.sidebar.header('Datos del paciente')
 st.subheader('Training Data Stats')
 st.write(df.describe())
 
@@ -35,13 +35,13 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random
 # FUNCTION
 def user_report():
   pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
-  glucose = st.sidebar.slider('Glucose', 0,200, 120 )
-  bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
+  glucose = st.sidebar.slider('Glucosa', 0,200, 120 )
+  bp = st.sidebar.slider('Presión arterial', 0,122, 70 )
   skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
-  insulin = st.sidebar.slider('Insulin', 0,846, 79 )
+  insulin = st.sidebar.slider('Insulina', 0,846, 79 )
   bmi = st.sidebar.slider('BMI', 0,67, 20 )
   dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
-  age = st.sidebar.slider('Age', 21,88, 33 )
+  age = st.sidebar.slider('Edad', 21,88, 33 )
 
   user_report_data = {
       'pregnancies':pregnancies,
@@ -61,7 +61,7 @@ def user_report():
 
 # PATIENT DATA
 user_data = user_report()
-st.subheader('Patient Data')
+st.subheader('Datos del paciente')
 st.write(user_data)
 
 
@@ -167,12 +167,12 @@ st.pyplot(fig_dpf)
 
 
 # OUTPUT
-st.subheader('Your Report: ')
+st.subheader('Reporte: ')
 output=''
 if user_result[0]==0:
-  output = 'You are not Diabetic'
+  output = 'No eres diabetico'
 else:
-  output = 'You are Diabetic'
+  output = 'Eres diabetico'
 st.title(output)
-st.subheader('Accuracy: ')
+st.subheader('Precisión: ')
 st.write(str(accuracy_score(y_test, rf.predict(x_test))*100)+'%')
